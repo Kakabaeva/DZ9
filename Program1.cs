376a0b3e7b4e2b8866d17f5e955ddf4d583e76c9
Эмина Какабаева <eminakakabaeva@MacBook-Air-Emina.local>
2023-04-08 20:41:57 +0300

    DZ9_2

---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+// Console.WriteLine("Введите первое натуральное число ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе натуральное число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// void ShowAllNumbersToN(int n)
+// {
+
+//     if (n == m) Console.Write("");
+//     else
+//     {
+//         Console.Write($"{n} ");
+//         ShowAllNumbersToN(n - 1);
+//     }
+
+
+
+// }
