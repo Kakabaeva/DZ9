ce50b14b7c9d1272c4f68550b41bbbf4ab362c5d
Эмина Какабаева <eminakakabaeva@MacBook-Air-Emina.local>
2023-04-09 21:10:16 +0300

    DZ9_1

--- Program1.cs
+++ Program1.cs
@@ -3,21 +3,31 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
-// Console.WriteLine("Введите первое натуральное число ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе натуральное число ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое натуральное число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число больше первого ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowAllNumbersToN(m);
 
-// void ShowAllNumbersToN(int n)
-// {
+void ShowAllNumbersToN(int m)
+{
 
-//     if (n == m) Console.Write("");
-//     else
-//     {
-//         Console.Write($"{n} ");
-//         ShowAllNumbersToN(n - 1);
-//     }
+    if (m%2==0) 
+    { 
+        if (n==m | n==(m+1)) Console.WriteLine($" {m} ");
+        else
+        {
+            Console.Write($" {m} ");
+            ShowAllNumbersToN(m+2);
+        }
+    }
+    if (m%2!=0) 
+    {
+        m=m+1;
+        Console.Write($" {m} ");
+            ShowAllNumbersToN(m+2);
+            
+    }
 
 
-
-// }
+}
